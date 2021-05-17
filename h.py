41fcc132d52153a5579637de6733b5ad12a160c0
Yiklek <yiklek.me@gmail.com>
2021-05-18 01:08:38 +0800

    [Improve] add uninstall command

--- h.py
+++ h.py
@@ -10,12 +10,16 @@ import os
 import sys
 import venv
 from os.path import join, isfile, isdir, abspath
+import shutil
 
 basedir = abspath(os.path.dirname(__file__))
 homedir = abspath(os.getenv('HOME'))
 xdg_config_dir = os.getenv('XDG_CONFIG_HOME')
+xdg_cache_dir = os.getenv('XDG_CACHE_HOME')
 ovim_dir = join(homedir, '.cache', 'ovim')
 vim_py3_env_dir = join(ovim_dir, 'python3-venv')
+cache_dir = xdg_cache_dir or join(homedir,'.cache')
+ovim_cache_dir = join(cache_dir,'ovim')
 
 if os.name == 'nt':
     lib = 'Lib'
@@ -59,7 +63,7 @@ def install(parser, args):
         vim_config_init = join(vim_config_path, 'init.vim')
 
     os.makedirs(vim_config_path, exist_ok=True)
-
+    print('create path %s successfully.')
     if not isfile(vim_config_init):
         os.symlink(join(basedir, 'vimrc'), vim_config_init)
     if not isfile(join(vim_config_path, 'autoload', 'plug.vim')):
@@ -82,9 +86,33 @@ def install(parser, args):
         a.func(parser, a)
 
 
+def uninstall(parser, args):
+    target = args.target
+    global config_dir
+    global ovim_cache_dir
+    vim_config_path = ''
+    if target == 'vim':
+        vim_config_path = join(homedir, '.vim')
+    elif target == 'nvim':
+        vim_config_path = join(config_dir, 'nvim')
+    try:
+        shutil.rmtree(vim_config_path)
+        print("delete config dir %s successfully." % vim_config_path)
+    except:
+        print("delete config dir %s failed. please remove %s manually." % (vim_config_path, vim_config_path))
+    if args.remove_cache:
+        try:
+            shutil.rmtree(ovim_cache_dir)
+            print("delete config dir %s successfully." % ovim_cache_dir)
+        except:
+            print("delete config dir %s failed. please remove %s manually." % (ovim_cache_dir, ovim_cache_dir))
+
+
+
 def create_arg_parser():
     parser = argparse.ArgumentParser(description='vim config helper.')
     subparsers = parser.add_subparsers(metavar="COMMAND", dest='command')
+    # install
     parser_install = subparsers.add_parser(
         'install', aliases=['i'], help='install vim config')
     parser_install.set_defaults(func=install)
@@ -92,6 +120,14 @@ def create_arg_parser():
         'vim', 'nvim'], default='vim', help='option choices: vim, nvim. default: vim', nargs='?')
     parser_install.add_argument('-d', '--depend', dest='install_depend', type=str,
                                 action='append', nargs='?', help='install dependency.example: -d="-i-p" -d="-n"')
+    # uninstall
+    parser_uninstall = subparsers.add_parser('uninstall', aliases=['u'], help='uninstall vim config')
+    parser_uninstall.set_defaults(func=uninstall)
+    parser_uninstall.add_argument('target', metavar='TARGET', choices=[
+        'vim', 'nvim'], default='vim', help='option choices: vim, nvim. default: vim', nargs='?')
+    parser_uninstall.add_argument('-r-c', '--remove-cache', dest='remove_cache',
+                            help='remove cache. default: False', default=False, action='store_true')
+    # depend
     # create the parser for the "b" command
     parser_dep = subparsers.add_parser(
         'depend', aliases=['d'], help='install vim dependency')
