03dc7522664ccedfad5e00373699d0aa994cee39
Yiklek <yiklek.me@gmail.com>
2022-04-16 20:58:15 +0800

    refactor(cmp): change scroll keymaps

--- ovim/lua/ovim/modules/completion/config.lua
+++ ovim/lua/ovim/modules/completion/config.lua
@@ -95,8 +95,8 @@ function C.nvim_cmp()
             ["<CR>"] = cmp.mapping.confirm({select = true}),
             ["<C-p>"] = cmp.mapping.select_prev_item(),
             ["<C-n>"] = cmp.mapping.select_next_item(),
-            ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-            ["<C-f>"] = cmp.mapping.scroll_docs(4),
+            ["<C-k>"] = cmp.mapping.scroll_docs(-4),
+            ["<C-j>"] = cmp.mapping.scroll_docs(4),
             ["<C-e>"] = cmp.mapping.close(),
             ["<Tab>"] = cmp.mapping(
                 function(fallback)
