3333fb0cab953b5c6b6976694914e013f04ecf44
Yiklek <yiklek.me@gmail.com>
2022-05-02 00:01:16 +0800

    refactor(search): telescope keymap

--- ovim/lua/ovim/modules/search/keymap.lua
+++ ovim/lua/ovim/modules/search/keymap.lua
@@ -13,6 +13,7 @@ function K.telescope()
         ["n|<leader>st"] = display("Telescope"),
         ["n|<leader>st<space>"] = map_cr("Telescope"):with_display():with_noremap():with_silent(),
         ["n|<leader>stf"] = map_cr("Telescope frecency"):with_display():with_noremap():with_silent(),
+        ["n|<leader>stc"] = map_cr("Telescope commands"):with_display():with_noremap():with_silent(),
     }
 end
 
