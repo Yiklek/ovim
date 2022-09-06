7d5b1c34759eada4e635df10f1a4238c4f189a6c
Yiklek <yiklek.me@gmail.com>
2022-09-06 20:57:43 +0800

    fix: file tree. nvim-tree disable update_focused_file coc-explorer help map g?

--- ovim/lua/ovim/modules/ui/config.lua
+++ ovim/lua/ovim/modules/ui/config.lua
@@ -232,11 +232,6 @@ function C.nvim_tree()
             enable = false,
             icons = {hint = "", info = "", warning = "", error = ""}
         },
-        update_focused_file = {
-            enable = true,
-            update_cwd = true,
-            ignore_list = {}
-        },
         system_open = {cmd = nil, args = {}},
         filters = {dotfiles = false, custom = {}},
         git = {enable = true, ignore = true, timeout = 500},
