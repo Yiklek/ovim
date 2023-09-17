8d66a84aba70022e38f57c25420c1c003b4449c9
Yiklek <yiklek.me@gmail.com>
2023-09-17 15:18:34 +0800

    feat(fmt): guard-collection

--- ovim/lua/ovim/modules/editor/features.lua
+++ ovim/lua/ovim/modules/editor/features.lua
@@ -149,18 +149,19 @@ return {
         ft("cpp"):fmt("clang-format")
         ft("lua"):fmt("stylua")
         ft("python"):fmt("black")
-        ft("cmake"):fmt("cmake-format")
-        ft("json"):fmt("prettier")
-        -- require("guard").setup {
-        --   -- the only options for the setup function
-        --   fmt_on_save = false,
-        --   -- Use lsp if no formatter was defined for this filetype
-        --   lsp_as_default_formatter = true,
-        -- }
+        -- ft("cmake"):fmt("cmake-format")
+        ft("typescript,javascript,typescriptreact,json"):fmt("prettier")
+        require("guard").setup {
+          -- the only options for the setup function
+          fmt_on_save = false,
+          -- Use lsp if no formatter was defined for this filetype
+          lsp_as_default_formatter = true,
+        }
       end,
       keys = km.to_lazy {
         ["n|<leader>xf"] = km.map_cmd("GuardFmt"):display("Format"),
       },
+      dependencies = { "nvimdev/guard-collection" },
     }
   end,
 }
