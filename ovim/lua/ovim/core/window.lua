26beeb00ff9c0d5eca012d3cc0921a8865e3d539
Yiklek <yiklek.me@gmail.com>
2024-08-16 01:40:45 +0800

    fix: close floating window error

--- ovim/lua/ovim/core/window.lua
+++ ovim/lua/ovim/core/window.lua
@@ -284,7 +284,9 @@ function M.remove_window(window)
   if type(window) == "table" then
     window = window.win
   end
-  vim.api.nvim_win_close(window, true)
+  if vim.api.nvim_win_is_valid(window) then
+    vim.api.nvim_win_close(window, false)
+  end
   local find = vim.tbl_filter(function(w)
     return w.win == window
   end, M._wins)
