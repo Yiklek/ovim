95df6ff77f2044d895773b8a681d3029b57398bf
Yiklek <yiklek.me@gmail.com>
2025-03-30 01:09:09 +0800

    remove deprecated api

--- ovim/lua/ovim/modules/lsp/lsp_config.lua
+++ ovim/lua/ovim/modules/lsp/lsp_config.lua
@@ -74,10 +74,6 @@ if vim.diagnostic ~= nil and vim.diagnostic.config ~= nil then
   vim.diagnostic.config {
     virtual_text = false,
   }
-else
-  vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-    virtual_text = false,
-  })
 end
 local keymap = require("ovim.modules.lsp.keymap")
 require("ovim.core.keymap").load(keymap.lsp())
